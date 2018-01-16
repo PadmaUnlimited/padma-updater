49ae9a098a33fc4e190ccab8d05e94eef4ed6f28
Manfred <marodok@gmail.com>
2018-01-16 10:20:37 -0600

    Base del plugin

---
+++ padma-updater.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name:	Padma Updater
+Plugin URI:		https://padmaunlimited/plugins/padma-updater
+Description:  	Padma Updater plugin allows to your website to access and update Padma Theme and Padma Plugins
+Version:      	0.0.1
+Author:       	Plasma Soluciones
+Author URI:   	https://plasma.cr
+License:      	GPL2
+License URI:  	https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  	padma-updater
+Domain Path:  	/languages
+
+
+Padma Updater plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Padma Updater plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Padma Updater plugin. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
